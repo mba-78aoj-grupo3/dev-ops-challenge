2f7bc6094409e522d4f99f8ec8befcc8fa01651d
gutiago <gu.tiagosilva@gmail.com>
2021-06-28 15:10:14 -0300

    adding terraform example

---
+++ terraform/main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  version = "~> 2.0"
+
+  region  = var.region
+}
+
+variable "region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+// CREATE SNS TOPIC
+resource "aws_sns_topic" "requests" {
+    name = "requests-topic"
+}
+
+// CREATE EMAIL TOPIC SUBSCRIPTION
+resource "aws_sns_topic_subscription" "requests_email_target" {
+  topic_arn = "${aws_sns_topic.requests.arn}"
+  protocol  = "email"
+  endpoint  = "rm340169@fiap.com.br"
+}
+
+// CREATE SQS QUEUE AND CONNECT WITH DLQ
+resource "aws_sqs_queue" "requests_queue" {
+    name = "requests-queue"
+    redrive_policy  = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.requests_dl_queue.arn}\",\"maxReceiveCount\":2}"
+    visibility_timeout_seconds = 120
+
+    tags = {
+        Environment = "production"
+    }
+}
+
+// CREATE DLQ 
+resource "aws_sqs_queue" "requests_dl_queue" {
+    name = "requests-dl-queue"
+}
+
+// CREATE SNS -> SQS TOPIC SUBSCRIPTION
+resource "aws_sns_topic_subscription" "requests_sqs_target" {
+    topic_arn = "${aws_sns_topic.requests.arn}"
+    protocol  = "sqs"
+    endpoint  = "${aws_sqs_queue.requests_queue.arn}"
+}
+
+// SQS POLICY
+resource "aws_sqs_queue_policy" "requests_queue_policy" {
+    queue_url = "${aws_sqs_queue.requests_queue.id}"
+
+    policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.requests_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.requests.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
